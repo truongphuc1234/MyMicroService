b38ba98c5b400a24be5d499aa4168ebe0e33dae5
Phuc Pham <pham.truongphuc8193@gmail.com>
2022-08-10 20:55:23 +0700

    Integrate API Gateway

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,14 +1,24 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
 builder.Services.AddOcelot();
+builder.Services.AddAuthentication().AddJwtBearer("Bearer",o =>
+{
+    o.RequireHttpsMetadata = false;
+    o.Authority = "http://localhost:5173";
+    o.TokenValidationParameters = new TokenValidationParameters { ValidateAudience = false };
+});
 var app = builder.Build();
 
 app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseOcelot().Wait();
 
 app.Run();
